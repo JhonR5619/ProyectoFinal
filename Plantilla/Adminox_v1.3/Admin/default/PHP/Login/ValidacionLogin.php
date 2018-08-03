2eb51268ad656912b4a599a841dc0ec563fd4222
Jhonatan Ramirez <jramirez485@misena.edu.co>
2018-08-03 11:50:12 -0500

    login con algo de programacion y listar

---
+++ Plantilla/Adminox_v1.3/Admin/default/PHP/Login/ValidacionLogin.php
@@ -0,0 +1,25 @@
+<?php
+  $Usuario=$POST['Usuario'];
+  $Contraseña=$POST['Contraseña'];
+
+  //Conectar a la base de datos
+  $_Session['Usuario']= $Usuario;
+
+
+  $conexion=mysqli_connect("localhost:8080","root","","BDVisitor");
+  $consulta="Select * from Funcionario where Usuario = '$Usuario' and Contraseña = '$Contraseña' ";
+  $resultado = mysqli_query($conexion,$consulta);
+  $filas = mysql_num_rows($resultado);
+
+  if($filas > 0 ){
+    header("location:index.html");
+  }else{
+    echo "error en la autentificacion, por favor verifique sus datos e intente nuevamente ";
+  }
+  mysql_free_result($resultado);
+  mysqli_close($conexion);
+
+
+
+
+ ?>
