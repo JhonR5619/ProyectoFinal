0ed1d16a7a5fe1e1c62533faf4df460eda26b964
Jhonatan Ramirez <jramirez485@misena.edu.co>
2018-08-09 11:30:14 -0500

    Login faltando un detalle y avances en registrar -alerta pendiente

--- Plantilla/Adminox_v1.3/Admin/default/PHP/Login/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $mysqli = new mysqli('localhost:8080','root','','DBVisitor');
-  if ($mysqli->connect_error) {
-   die("La conexion falló: " . $mysqli->connect_error);
-  }
-
-
- ?>
---
+++ Plantilla/Adminox_v1.3/Admin/default/PHP/Login/file.php
@@ -0,0 +1,32 @@
+<?php
+
+if ($_FILES['archivo']["error"] > 0) {
+
+  echo "Error";
+}else {
+  echo "Upload :" .$_FILES['archivo']['name'];
+  echo "Type :" .$_FILES['archivo']['type'];
+  echo "Size :" .$_FILES['archivo']['size'/1024]"kB <br>";
+  echo "Stored in :" .$_FILES['archivo']['tmp_name'];
+
+  move_uploaded_file($_FILES['archivo']['tmp_name'], '../ImagenesSubidas'.$_FILES['archivo']['tmp_name']);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
